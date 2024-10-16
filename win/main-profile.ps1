4b5e0e85edb937311b00445a9ee1cb5882c646a4
Florin Catalin Boldan <floring-catalin.boldan@limonetik.com>
2024-10-16 14:19:40 +0300

    changes to vim bindings, add PS profile and ohmypish config

---
+++ win/main-profile.ps1
@@ -0,0 +1,5 @@
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+oh-my-posh init pwsh | Invoke-Expression
